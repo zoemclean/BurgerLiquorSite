f13ce7849c0e0b8cb3029552461b2cdd9a64c049
Zoe McLean <zoe@Zoes-iMac.local>
2020-04-22 13:27:12 +1200

    finished coding my Burger Liquor Self ordering site

--- js/script.js
+++ js/script.js
@@ -1,3 +1,102 @@
 $('#navListButton').click(function(){
     $('#navList').slideToggle(300);
 });
+
+
+$('#standardBurger').click(function(){
+    $('#standardBurgerModal').removeClass('hidden').addClass('flex');
+    $('body').addClass('noScroll');
+});
+
+$('#closeBurg').click(function(){
+    $('#standardBurgerModal').addClass('hidden').removeClass('flex');
+    $('body').removeClass('noScroll');
+})
+
+
+$('#punjabiPoutine').click(function(){
+    $('#poutineModal').removeClass('hidden').addClass('flex');
+    $('body').addClass('noScroll');
+});
+
+$('#closeFries').click(function(){
+    $('#poutineModal').addClass('hidden').removeClass('flex');
+    $('body').removeClass('noScroll');
+})
+
+
+$('#rosebud').click(function(){
+    $('#rosebudModal').removeClass('hidden').addClass('flex');
+    $('body').addClass('noScroll');
+});
+
+$('#closeRose').click(function(){
+    $('#rosebudModal').addClass('hidden').removeClass('flex');
+    $('body').removeClass('noScroll');
+})
+
+
+$('#saltedCarRum').click(function(){
+    $('#saltedCarRumModal').removeClass('hidden').addClass('flex');
+    $('body').addClass('noScroll');
+});
+
+$('#closeSaltedRum').click(function(){
+    $('#saltedCarRumModal').addClass('hidden').removeClass('flex');
+    $('body').removeClass('noScroll');
+})
+
+
+$('#callWaitMobile').click(function(){
+    $('#callWaitMobileModal').removeClass('hidden').addClass('flex');
+    $('body').addClass('noScroll');
+});
+
+$('#closeCallMobile').click(function(){
+    $('#callWaitMobileModal').addClass('hidden').removeClass('flex');
+    $('body').removeClass('noScroll');
+})
+
+
+$('#callWaitMLrg').click(function(){
+    $('#callWaitLrgModal').removeClass('hidden').addClass('flex');
+    $('body').addClass('noScroll');
+});
+
+$('#closeCallLrg').click(function(){
+    $('#callWaitLrgModal').addClass('hidden').removeClass('flex');
+    $('body').removeClass('noScroll');
+})
+
+
+$('#review').click(function(){
+    $('#reviewModal').removeClass('hidden').addClass('flex');
+    $('body').addClass('noScroll');
+});
+
+$('#closeReview').click(function(){
+    $('#reviewModal').addClass('hidden').removeClass('flex');
+    $('body').removeClass('noScroll');
+})
+
+
+$('#placeOrder').click(function(){
+    $('#placeOrderModal').removeClass('hidden').addClass('flex');
+    $('body').addClass('noScroll');
+});
+
+$('#closePlaceOrder').click(function(){
+    $('#placeOrderModal').addClass('hidden').removeClass('flex');
+    $('body').removeClass('noScroll');
+})
+
+
+$('#yourCart').click(function(){
+    $('#cartModal').removeClass('hidden').addClass('flex');
+    $('body').addClass('noScroll');
+});
+
+$('#closeCart').click(function(){
+    $('#cartModal').addClass('hidden').removeClass('flex');
+    $('body').removeClass('noScroll');
+})
